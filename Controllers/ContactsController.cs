a48e8a82de8be7d88284a68878d4f26170dd6856
mick2025 <142557072+mick2025@users.noreply.github.com>
2023-08-18 09:35:33 -0400

    Initial upload

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,58 @@
+﻿using HubSpotApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Text;
+
+namespace HubSpotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly string _token;
+        private readonly string _baseUrl;
+        private readonly string _endPoint;
+        
+
+        public ContactsController(IConfiguration configuration)
+        {
+            _baseUrl = configuration["HubSpotBaseUrl"];
+            _endPoint = configuration["HubSpotContactEndpoint"];
+            _token = configuration["HubSpotToken"];
+        }
+
+        [HttpPost]
+        public async void AddContact([FromBody] Contact request)
+        {            
+            try
+            {
+                var contacObj = new Properties();
+                contacObj.properties = request;
+                var data = JObject.FromObject(contacObj);
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_baseUrl);
+
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _token);
+
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var response = client.PostAsync(_endPoint, new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")).Result;
+
+                    response.EnsureSuccessStatusCode();
+                    
+                }
+            }   
+            catch (Exception ex)
+            {
+                // nothing yet                
+            }
+            
+        }
+
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿namespace HubSpotApi.Models
+{
+    public class Contact
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string platform { get; set; }
+        public string contact_source { get; set; }
+        public string contact_type { get; set; }
+        public string hs_lead_status { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+        public int campaign_id { get; set; }
+
+    }
+
+    public class Properties
+    {
+        public Contact properties { get; set; }
+    }
+}
